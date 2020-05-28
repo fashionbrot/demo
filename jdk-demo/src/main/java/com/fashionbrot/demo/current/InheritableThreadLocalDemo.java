a10f1f4014afadc723175432087d5b2e876fae74
fashionbrot@163.com <fashionbrot@163.com>
2020-05-28 23:01:55 +0800

    修改demo

--- jdk-demo/src/main/java/com/fashionbrot/demo/current/InheritableThreadLocalDemo.java
+++ jdk-demo/src/main/java/com/fashionbrot/demo/current/InheritableThreadLocalDemo.java
@@ -1,5 +1,8 @@
 package com.fashionbrot.demo.current;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 子线程获取主线程 值
  *
@@ -13,9 +16,17 @@ public class InheritableThreadLocalDemo {
     public static void main(String args[]){
         threadLocal.set(new Integer(123));
 
+        ExecutorService executorService =Executors.newFixedThreadPool(10);
+
         for (int i=0;i<10;i++) {
             Thread thread = new MyThread();
             thread.start();
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("ExecutorService threadLocal:"+threadLocal.get());
+                }
+            });
         }
         System.out.println("main = " + threadLocal.get());
     }
